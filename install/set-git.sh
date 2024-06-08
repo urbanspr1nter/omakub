155cdbedb9a04441d84ad12e88393a102330434a
Roger Ngo <urbanspr1nter@gmail.com>
2024-06-07 20:20:04 -0700

    Removing unwanted apps

--- install/set-git.sh
+++ install/set-git.sh
@@ -4,3 +4,7 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global pull.rebase true
+git config --global user.name "Roger Ngo"
+git config --global user.email "urbanspr1nter@gmail.com"
+git config --global core.autocrlf false
+git config --glboal core.eol lf
