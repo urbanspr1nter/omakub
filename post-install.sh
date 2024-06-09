fd7253320fb085dc4f0f9bf1d900c1e27ee44bd2
Roger Ngo <urbanspr1nter@gmail.com>
2024-06-09 04:42:32 -0700

    Make git more customizable

--- post-install.sh
+++ post-install.sh
@@ -1,1 +1,8 @@
 mkdir -p ~/code
+
+# set git stuff
+read -p "Enter your name to be shown in git commits " GIT_USERNAME
+git config --global user.name "$GIT_USERNAME"
+
+read -p "Enter your email to be entered in git commits " GIT_EMAIL
+git config --global user.email "$GIT_EMAIL"
